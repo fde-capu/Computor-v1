a595f50465207623c8471e5e243aca2d3fb3de1c
Flavio Carrara De Capua <flavio@carrara.us>
2024-02-01 09:29:19 -0300

    ...yes, there is error. Checking.

--- permutations.py
+++ permutations.py
@@ -3,9 +3,18 @@
 import itertools
 import random
 import numpy as np
+import sys
 
 characters = ['+', '-', '0', ' ']
 random_max = 100
+
+if len(sys.argv) != 4:
+    print("Needs a, b, and c as arguments.")
+    sys.exit(1)
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+c = float(sys.argv[3])
 
 # Use itertools.product to generate all permutations
 perms = list(itertools.product(characters, repeat=3))
@@ -35,48 +44,47 @@ def map_chars(char, t):
 integers_perms = [[map_chars(char, 'integer') for char in perm] for perm in perms]
 floating_perms = [[map_chars(char, 'floatin') for char in perm] for perm in perms]
 
-# Print each mapped permutation in a separate line
-for perm in integers_perms:
-  print(perm)
+#for perm in integers_perms:
+#  print(perm)
 
-for perm in floating_perms:
-  print(perm)
+#for perm in floating_perms:
+#  print(perm)
 
 def build_polynomial(element):
-	a = element[0]
-	b = element[1]
-	c = element[2]
-	if a != '?':
-		a_sig = '+ ' if a >= 0 else '- '
-		a_abs = abs(a)
+	e_a = element[0]
+	e_b = element[1]
+	e_c = element[2]
+	if e_a != '?':
+		a_sig = '+ ' if e_a >= 0 else '- '
+		a_abs = abs(e_a)
 		a_str = f'{a_sig}{a_abs}x^2 '
 	else:
 		a_str = ''
-		a = 0
-	if b != '?':
-		b_sig = '+' if b >= 0 else '-'
-		b_abs = abs(b)
+		e_a = 0
+	if e_b != '?':
+		b_sig = '+' if e_b >= 0 else '-'
+		b_abs = abs(e_b)
 		b_str = f'{b_sig} {b_abs}x '
 	else:
 		b_str = ''
-		b = 0
-	if c != '?':
-		c_sig = '+' if c >= 0 else '-'
-		c_abs = abs(c)
+		e_b = 0
+	if e_c != '?':
+		c_sig = '+' if e_c >= 0 else '-'
+		c_abs = abs(e_c)
 		c_str = f'{c_sig} {c_abs}'
 	else:
 		c_str = ''
-		c = 0
+		e_c = 0
 	f_str = f'{a_str}{b_str}{c_str} = 0'
 	f_str = f_str[2:] if f_str.startswith('+ ') else f_str
 	# Calculate the roots
-	discriminant = b**2 - 4*a*c
+	discriminant = e_b**2 - 4*e_a*e_c
 	if discriminant >= 0:
-		root1 = (-b + np.sqrt(discriminant)) / (2*a)
-		root2 = (-b - np.sqrt(discriminant)) / (2*a)
+		root1 = (-e_b + np.sqrt(discriminant)) / (2*e_a)
+		root2 = (-e_b - np.sqrt(discriminant)) / (2*e_a)
 	else:
-		real_part = -b / (2*a)
-		imaginary_part = np.sqrt(-discriminant) / (2*a)
+		real_part = -e_b / (2*e_a)
+		imaginary_part = np.sqrt(-discriminant) / (2*e_a)
 		root1 = complex(real_part, imaginary_part)
 		root2 = complex(real_part, -imaginary_part)
 	return f_str, [root1, root2], discriminant
@@ -99,13 +107,12 @@ def interpret_result(roots, discriminant):
 			f'{str_r0}+{abs(roots[0].imag):.5f}i\n{str_r1}-{abs(roots[1].imag):.5f}i'
 
 
-element = integers_perms[0]
-poly_string, poly_roots, poly_discriminant = build_polynomial(element)
-print(poly_string, f'-> delta: {poly_discriminant} -> y =', poly_roots)
+#element = integers_perms[0]
+#poly_string, poly_roots, poly_discriminant = build_polynomial(element)
+#print(poly_string, f'-> delta: {poly_discriminant} -> y =', poly_roots)
 
-manual = [1, 0, 6]
-poly_string, poly_roots, poly_discriminant = build_polynomial(manual)
+args = [a, b, c]
+poly_string, poly_roots, poly_discriminant = build_polynomial(args)
 print(poly_string, f'-> delta: {poly_discriminant} -> y =', poly_roots)
-
 print(interpret_result(poly_roots, poly_discriminant))
 

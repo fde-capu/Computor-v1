f0e82323b36f26e3d1ed039a9b39f98544b58dd2
Flavio Carrara De Capua <flavio@carrara.us>
2024-02-15 09:39:38 -0300

    Substituting "\d \d" for "\d*\d" just because "x 3" and "x3" are "x*3"

--- Computor_v1_treats.cpp
+++ Computor_v1_treats.cpp
@@ -4,9 +4,9 @@ void Computor_v1::treat_spaces()
 {
 	int V(2);
 
-	verbose(V) << "treat_spaces:" << std::endl;
+	verbose(V) << "treat_spaces input: " << this->treating << std::endl;
 	substitute_unloop(this->treating, "X", "x");
-	substitute_unloop(this->treating, "\\t", " ");
+	substitute_unloop(this->treating, "\\s", " ");
 	substitute_unloop(this->treating, "*", " * ");
 	substitute_unloop(this->treating, "/", " / ");
 	substitute_unloop(this->treating, "+", " + ");
@@ -20,8 +20,9 @@ void Computor_v1::treat_spaces()
 	substitute_super(this->treating, " /", "/");
 	substitute_super(this->treating, "- ", "-");
 	substitute_super(this->treating, "+ ", "+");
+	substitute_super(this->treating, "  ", " ");
 	hard_trim(this->treating);
-	verbose(V) << this->treating << std::endl;
+	verbose(V) << "treat_spaces result: " << this->treating << std::endl;
 }
 
 void Computor_v1::mount_terms()
@@ -33,6 +34,11 @@ void Computor_v1::mount_terms()
 	substitute_super(this->treating, "x x", "xx");
 	substitute_super(this->treating, " x", "x");
 	substitute_super(this->treating, ".x", "x");
+	for (size_t i = 0; i < 10; i++)
+	{
+		for (size_t j = 0; j < 10; j++)
+			substitute_super(this->treating, itoa(i) + " " + itoa(j), itoa(i) + "*" + itoa(j));
+	}
 	verbose(V) << this->treating << std::endl;
 	std::pair<size_t, size_t> x_count = find_char_sequence(this->treating, 'x');
 	while (x_count.first != std::string::npos)
@@ -62,6 +68,7 @@ void Computor_v1::treat_implicits()
 {
 	int V(2);
 
+	verbose(V) << "(treat_implicits) input: " << this->terms << std::endl;
 	for (auto& c : this->terms)
 	{
 		substitute_unloop(c, "0x", "0*x");
@@ -92,7 +99,7 @@ void Computor_v1::treat_implicits()
 		substitute_unloop(c, "--", "+"); // Again.
 		substitute_unloop(c, "++", "+"); // Again.
 	}
-	verbose(V) << "(treat_implicits): " << this->terms << std::endl;
+	verbose(V) << "(treat_implicits) result: " << this->terms << std::endl;
 }
 
 void Computor_v1::validate_terms()
